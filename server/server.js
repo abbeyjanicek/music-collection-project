f498d0042182a2b95828e623835055fc8261b646
Abbey Janicek <abbey.janicek@gmail.com>
2018-08-23 12:12:06 -0500

    Server is working.

---
+++ server/server.js
@@ -0,0 +1,24 @@
+//VARIABLES/REQUIRES
+const express = require ('express');
+const app = express ();
+const PORT = process.env.PORT || 5000;
+const bodyParser = require ('body-parser');
+//const musicRouter = require('./routes/music.router.js');
+//const genreRouter = require('./routes/genre.router.js');
+
+
+//USES
+//body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//set-up routes
+//app.use('/music', musicRouter);
+//app.use('/genre', genreRouter);
+
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log('server running on port: ', PORT);
+    
+});
